a191c5eec3f364727c096506991f184235862805
JoseIgnacioTapia <josignaciot@gmail.com>
2023-12-13 13:03:00 -0300

    Project Setup - Routes - Layouts - Links

---
+++ app/meals/[mealSlug]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MealDetailsPage() {
+  return <div>Meal Details Page</div>;
+}
+
+export default MealDetailsPage;
---
+++ components/main-header.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Link from "next/link";
+
+import logoImg from "@/assets/logo.png";
+
+function MainHeader() {
+  return (
+    <header>
+      <Link href="/">
+        <img src={logoImg} alt="A plate with food on it" />
+        NextLevel Food
+      </Link>
+
+      <nav>
+        <ul>
+          <li>
+            <Link href="/meals">Browse Meals</Link>
+          </li>
+          <li>
+            {" "}
+            <Link href="/community">Foodies communities</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainHeader;
