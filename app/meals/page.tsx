7b514ce825c431552a25076eff5b0388968df859
JoseIgnacioTapia <josignaciot@gmail.com>
2023-12-16 19:09:32 -0300

    Outputting Meals Data & Images With Unknown Dimensions

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
+import classes from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
+
 function MealsPage() {
-  return <h1>MealsPage</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite meal from our broad selection of available meals
+          and enjoy a delicious lunch or dinner at home.
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipes</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
 
 export default MealsPage;
