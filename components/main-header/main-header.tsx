2aae25faae92c34fac4005909e2aff8f47098425
JoseIgnacioTapia <josignaciot@gmail.com>
2023-12-15 21:25:48 -0300

    Main header components

---
+++ components/main-header/main-header.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+import MainHeaderBackground from "./main-header-background";
+
+import classes from "./main-header.module.css";
+import logoImg from "@/assets/logo.png";
+
+function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              {" "}
+              <Link href="/community">Foodies communities</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default MainHeader;
