fb93ac1c499b9fcf804e3284cc68c5de85ade558
JoseIgnacioTapia <josignaciot@gmail.com>
2023-12-16 18:34:23 -0300

    Using Client Components Efficiently

--- components/main-header/main-header.tsx
+++ components/main-header/main-header.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 
 import classes from "./main-header.module.css";
 import logoImg from "@/assets/logo.png";
@@ -21,11 +22,10 @@ function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              {" "}
-              <Link href="/community">Foodies communities</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
