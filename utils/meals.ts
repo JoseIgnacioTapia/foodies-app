342b1b79c755d1ece64c30b41a7ce0bb8be049bd
JoseIgnacioTapia <josignaciot@gmail.com>
2023-12-31 19:31:17 -0300

    Creating a Slug & Sanitizing User Input for XSS Protection

--- utils/meals.ts
+++ utils/meals.ts
@@ -1,7 +1,10 @@
 import sql from "better-sqlite3";
 import { resolve } from "path";
+import slugify from "slugify";
+import xss from "xss";
 
 import { Meal } from "@/types";
+import MealItem from "../components/meals/meal-item";
 
 const db = sql("meals.db");
 
@@ -17,4 +20,9 @@ export async function getMeals(): Promise<Meal[]> {
 export function getMeal(slug: string): Meal | undefined {
   const result = db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
   return result as Meal | undefined;
+}
+
+export function saveMeal(meal: Meal) {
+  meal.slug = slugify(meal.title, { lower: true });
+  meal.instructions = xss(meal.instructions);
 }
