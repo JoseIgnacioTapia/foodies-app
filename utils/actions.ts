879b65ec290e911f65225466848cc13263f34c32
JoseIgnacioTapia <josignaciot@gmail.com>
2024-01-04 10:55:37 -0300

    Adding Server-Side Input Validation

--- utils/actions.ts
+++ utils/actions.ts
@@ -4,16 +4,35 @@ import { Meal } from "@/types";
 import { saveMeal } from "./meals";
 import { redirect } from "next/navigation";
 
-export async function shareMeal(formData: FormData) {
-  const meal = {
-    title: formData.get("title"),
-    summary: formData.get("summary"),
-    instructions: formData.get("instructions"),
-    image: formData.get("image"),
-    creator: formData.get("name"),
-    creator_email: formData.get("email"),
+function isInvalidText(text: string | null | undefined): boolean {
+  return !text || text.trim() === "";
+}
+
+export async function shareMeal(formData: FormData): Promise<void> {
+  const meal: Meal = {
+    title: formData.get("title") as string,
+    summary: formData.get("summary") as string,
+    instructions: formData.get("instructions") as string,
+    image: formData.get("image") as File,
+    creator: formData.get("name") as string,
+    creator_email: formData.get("email") as string,
     slug: undefined,
   };
+
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.includes("@") ||
+    !meal.image ||
+    !(meal.image instanceof File) ||
+    meal.image.size === undefined ||
+    meal.image.size === 0
+  ) {
+    throw new Error("Invalid input");
+  }
 
   await saveMeal(meal as Meal);
   redirect("/meals");
